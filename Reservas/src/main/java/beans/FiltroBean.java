fb6b6fd39d3550a5b3883743d88e20f10595c709
cristixn_93@hotmail.com <cristixn_93@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2014-09-01 22:18:37 +0000

    filtro y beans

---
+++ Reservas/src/main/java/beans/FiltroBean.java
@@ -0,0 +1,60 @@
+package Bean;
+
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+import java.util.Date;
+
+
+
+@ManagedBean(name="FiltroBean")
+@SessionScoped
+
+public class FiltroBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Date fecha;
+	private Date hora;
+	private String software;
+	private int numero_estudiantes;
+	
+	
+	
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Date getHora() {
+		return hora;
+	}
+
+	public void setHora(Date hora) {
+		this.hora = hora;
+	}
+
+	public String getSoftware() {
+		return software;
+	}
+
+	public void setSoftware(String software) {
+		this.software = software;
+	}
+
+	public int getNumero_estudiantes() {
+		return numero_estudiantes;
+	}
+
+	public void setNumero_estudiantes(int numero_estudiantes) {
+		this.numero_estudiantes = numero_estudiantes;
+	}
+
+
+}
---
+++ Reservas/src/main/java/beans/HomeBean.java
@@ -0,0 +1,39 @@
+package Bean;
+
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+
+@ManagedBean(name="HomeBean")
+@SessionScoped
+
+public class HomeBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String usuario;
+	private String password;
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
+
+
+
