70a39f213ebd26a0c35aef9e00a95098c1bd6ee1
stjimmy.4@hotmail.com <stjimmy.4@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2014-09-02 01:31:55 +0000

    Variables deshabi

--- trunk/Reservas/src/main/java/beans/SalasBean.java
+++ trunk/Reservas/src/main/java/beans/SalasBean.java
@@ -20,9 +20,21 @@ public class SalasBean implements Serializable {
     boolean e5;
     boolean e6;
     boolean e7;
+    
+    
+    FiltroBean f=new FiltroBean();
   public boolean gete1() {
+      
+    /*  if(f.getNumero_estudiantes()>20)
+          e1=true;
+      else
+          e1=false;
 		return e1;
-	}
+            
+            */
+      return e1;
+  }
+            
 
 	public void sete1(boolean e1) {
 		this.e1 = e1;
