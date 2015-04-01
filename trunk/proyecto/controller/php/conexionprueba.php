530a617b85fa44aeca2ab0c2d8493db2800134c0
stjimmy.4@hotmail.com <stjimmy.4@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2015-04-01 21:40:01 +0000

    Michael conexion prueba

--- trunk/proyecto/controller/php/conexionprueba.php
+++ trunk/proyecto/controller/php/conexionprueba.php
@@ -1,22 +1,48 @@
 <?php
+
 class conexionprueba {
-    
-private $DBH ;
-function getMysql(){
-    
-    
-}
-function setMysql(){
-    
-    
-}
-function getSQLite(){
-    
-    
-}
-function setSQLite(){
-     $DBH = new PDO("sqlite:my/database/path/database.db");
-    
-}
+
+    private $DBH;
+    private $sentencia;
+
+    function setMysql($host, $dbname, $user, $pass) {
+
+        $this->DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    }
+
+    function setSQLite($database) {
+        $this->DBH = new PDO("sqlite:my/database/path/" . $database . ".db");
+    }
+
+    function getMysql() {
+
+        return $this->DBH;
+    }
+
+    function getSQLite() {
+
+        return $this->DBH;
+    }
+
+    function close() {
+
+        $this->DBH = null;
+    }
+
+    function execute() {
+
+        $f = $this->DBH->prepare($this->sentencia);
+        $f->execute();
+        $result = $f->fetchAll();
+        return $result;
+    }
+
+    function getStatement() {
+        return $this->sentencia;
+    }
+
+    function setStatement($sentencia) {
+        $this->sentencia = $sentencia;
+    }
 
 }
