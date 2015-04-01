4aa52160a83837e82c9cb46aee8c822e2cfde573
stjimmy.4@hotmail.com <stjimmy.4@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2015-04-01 18:51:03 +0000

    Crear proyecto

---
+++ trunk/proyecto/controller/files/prestamoDTO.php
@@ -0,0 +1,133 @@
+<?php
+ class prestamoDTO{
+private $id;
+private $fecha_entrega;
+private $fecha_devolucion;
+private $multa;
+private $estado;
+private $ejemplar_id;
+private $cliente_id;
+
+function getid(){
+ return $this->id;
+}
+
+function setid($id){
+ $this->id= $id;
+}
+
+function getfecha_entrega(){
+ return $this->fecha_entrega;
+}
+
+function setfecha_entrega($fecha_entrega){
+ $this->fecha_entrega= $fecha_entrega;
+}
+
+function getfecha_devolucion(){
+ return $this->fecha_devolucion;
+}
+
+function setfecha_devolucion($fecha_devolucion){
+ $this->fecha_devolucion= $fecha_devolucion;
+}
+
+function getmulta(){
+ return $this->multa;
+}
+
+function setmulta($multa){
+ $this->multa= $multa;
+}
+
+function getestado(){
+ return $this->estado;
+}
+
+function setestado($estado){
+ $this->estado= $estado;
+}
+
+function getejemplar_id(){
+ return $this->ejemplar_id;
+}
+
+function setejemplar_id($ejemplar_id){
+ $this->ejemplar_id= $ejemplar_id;
+}
+
+function getcliente_id(){
+ return $this->cliente_id;
+}
+
+function setcliente_id($cliente_id){
+ $this->cliente_id= $cliente_id;
+}
+
+}<?php
+ class prestamoDTO{
+private $id;
+private $fecha_entrega;
+private $fecha_devolucion;
+private $multa;
+private $estado;
+private $ejemplar_id;
+private $cliente_id;
+
+function getid(){
+ return $this->id;
+}
+
+function setid($id){
+ $this->id= $id;
+}
+
+function getfecha_entrega(){
+ return $this->fecha_entrega;
+}
+
+function setfecha_entrega($fecha_entrega){
+ $this->fecha_entrega= $fecha_entrega;
+}
+
+function getfecha_devolucion(){
+ return $this->fecha_devolucion;
+}
+
+function setfecha_devolucion($fecha_devolucion){
+ $this->fecha_devolucion= $fecha_devolucion;
+}
+
+function getmulta(){
+ return $this->multa;
+}
+
+function setmulta($multa){
+ $this->multa= $multa;
+}
+
+function getestado(){
+ return $this->estado;
+}
+
+function setestado($estado){
+ $this->estado= $estado;
+}
+
+function getejemplar_id(){
+ return $this->ejemplar_id;
+}
+
+function setejemplar_id($ejemplar_id){
+ $this->ejemplar_id= $ejemplar_id;
+}
+
+function getcliente_id(){
+ return $this->cliente_id;
+}
+
+function setcliente_id($cliente_id){
+ $this->cliente_id= $cliente_id;
+}
+
+}
---
+++ trunk/proyecto/generate.php
@@ -0,0 +1,147 @@
+<html>
+<head>
+<title>Problema</title>
+</head>
+<body>
+<?php
+  //leer archivos
+ $fichero_texto = fopen ("controller/files/SCRIPT.txt", "r");
+   //obtenemos de una sola vez todo el contenido del fichero
+   //OJO! Debido a filesize(), sólo funcionará con archivos de texto
+   $contenido_fichero = fread($fichero_texto, filesize("controller/files/SCRIPT.txt"));
+   $v=file("controller/files/SCRIPT.txt");
+   $cantidad = count($v);
+   $ot=0;
+   $aux=0;
+   $aux2="";
+   $var=$v;
+   $nombretablas;
+   $nombreatributos;
+   $atributos;
+   $atributoauxiliar;
+   $conatributos=0;
+   $cona=0;
+   $tipo="";
+   $concomillas=0;
+for($i=0;$i< $cantidad;$i++){
+   $con=0;
+  if (strpos($v[$i],'CREATE TABLE') !== false) {
+  $var="";
+     for($j=0;$j<strlen($v[$i]);$j++){
+        if($v[$i][$j]==="`"){
+           
+            $con++;
+        }
+         if($con>=3 &&$v[$i][$j]!='`'&& $v[$i][$j]!="(" ){
+             $var=$var.$v[$i][$j];
+        
+         }
+    }
+       
+   $nombretablas[$ot]=trim($var);
+   $ot=$ot+1;
+  $aux=$i+1;
+  $aux2="";
+  
+  
+  while(strpos($v[$aux],'PRIMARY KEY') === false) {
+     $concomillas=0;
+     $atributoauxiliar="";
+     
+       for($k=0;$k<strlen($v[$aux]);$k++){
+      // $aux2=$aux2.$v[$aux];
+       //echo $v[$aux][$k]." ".$cona."<br>";
+           if($v[$aux][$k]==="`"){
+               $concomillas++;
+               
+           }
+           if($concomillas===1 && $v[$aux][$k]!=="`"){
+             
+               $atributoauxiliar=$atributoauxiliar.$v[$aux][$k];
+           }
+           if($concomillas===2){
+               if($v[$aux][$k]==="I"){
+                   $tipo="int";
+                   
+               }
+               if($v[$aux][$k]==="V"){
+                   $tipo="String";
+                   
+               }
+               if($v[$aux][$k]==="D"){
+                   $tipo="float";
+                   
+               }
+               
+           }
+       
+      
+      
+       }
+       
+       $atributo[$conatributos][0]=$cona;
+       $atributo[$conatributos][1]=$atributoauxiliar;
+       $atributo[$conatributos][2]=$tipo;
+       
+       $conatributos++;
+       $aux++;
+   }
+   $cona++;
+    }
+    
+     
+    
+   
+}
+
+for($i=0;$i<$conatributos;$i++){
+    
+    for($j=0;$j<3;$j++){
+        
+        echo $atributo[$i][$j]." ";
+        
+    }
+    echo "<br>";
+}
+/*for($j=0;$j<  count($nombreatributos);$j++){
+    
+    echo $nombreatributos[$j]."<br>";
+   
+    
+}*/
+
+
+for($j=0;$j<  count($nombretablas);$j++){
+    echo$nombretablas[$j];
+    echo "<br>";
+    $ar=fopen("controller/files/".$nombretablas[$j]."DTO.php","a") or
+    die("Problemas en la creacion");
+    
+    fputs($ar,"<"."?"."php\n class ".$nombretablas[$j]."DTO{\n");
+    for($i=0;$i<$conatributos;$i++){
+    
+    
+        if($atributo[$i][0]===$j){
+        fputs($ar,"private $".$atributo[$i][1].";\n");
+        }
+    
+}
+  for($i=0;$i<$conatributos;$i++){
+    
+    
+        if($atributo[$i][0]===$j){
+        fputs($ar,"\nfunction get".$atributo[$i][1]."(){\n return"." $"."this->".$atributo[$i][1].";\n}\n");
+        fputs($ar,"\nfunction set".$atributo[$i][1]."($".$atributo[$i][1]."){\n $"."this->".$atributo[$i][1]."= $".$atributo[$i][1].";\n}\n");
+        }
+    
+}
+  fputs($ar,"\n}");
+ 
+  fclose($ar);
+}
+   
+ 
+
+  ?>
+</body>
+</html>
