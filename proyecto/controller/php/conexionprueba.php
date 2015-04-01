9513dbdf25f754f2f11c505074a060ad58d1c96e
stjimmy.4@hotmail.com <stjimmy.4@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2015-04-01 20:55:42 +0000

    Michael Generacion de DTOs dede la base de datos

---
+++ proyecto/controller/php/conexionprueba.php
@@ -0,0 +1,22 @@
+<?php
+class conexionprueba {
+    
+private $DBH ;
+function getMysql(){
+    
+    
+}
+function setMysql(){
+    
+    
+}
+function getSQLite(){
+    
+    
+}
+function setSQLite(){
+     $DBH = new PDO("sqlite:my/database/path/database.db");
+    
+}
+
+}
