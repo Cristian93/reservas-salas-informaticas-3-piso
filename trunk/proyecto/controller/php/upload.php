ff9f4281ab805589fd01c75b82cfbf1b4253e9dd
stjimmy.4@hotmail.com <stjimmy.4@hotmail.com@01cd54c8-c249-6867-9099-53e642ed4abc>
2015-04-08 23:37:46 +0000

    Archivo php para obtener el nombre y atributos del fichero cargado

---
+++ trunk/proyecto/controller/php/upload.php
@@ -0,0 +1,30 @@
+<?php
+
+//comprobamos que sea una petición ajax
+
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+
+    //obtenemos el archivo a subir
+    $file = $_FILES['archivo']['name'];
+
+
+    //comprobamos si existe un directorio para subir el archivo
+    //si no es así, lo creamos
+    //comprobamos si el archivo ha subido
+    if ($file && move_uploaded_file($_FILES['archivo']['tmp_name'], "../files/" . $file)) {
+         $fichero_texto = fopen ("../files/".$file, "r");
+
+        $contenido_fichero = fread($fichero_texto, filesize("../files/" . $file));
+        $ar = fopen("../files/SCRIPTAPL.txt", "a") or
+                die("Problemas en la creacion");
+        fputs($ar, $contenido_fichero);
+
+        fclose($ar);
+
+
+        echo "Archivo subido con exito<br>";
+        echo "<input type='button' value='Generar getters' onclick='Ongenerate()'/>";
+    }
+} else {
+    throw new Exception("Error Processing Request", 1);
+}
